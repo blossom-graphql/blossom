85383d0e72d42f5ac875b942268329b73e8bd52e
Sebastian Soto <spsoto@users.noreply.github.com>
2018-12-13 11:34:19 -0800

    Better Organization for Typescript

---
+++ packages/core/src/index.ts
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c) The Blossom GraphQL Team.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+export { blossom } from './blossom';
+
+export { createBlossomDecorators } from './instance';
+
+export { Maybe, MaybePromise } from './common';
+
+export { BlossomContext } from './context';
+
+export { deliver, deliverGroup } from './helpers';
+
+export { createResolver, createConnectionResolver, resolve } from './resolver';
