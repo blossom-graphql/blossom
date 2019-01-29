34062de1f27bad179e1f135c75868615d0cc5916
Sebastian Soto <spsoto@users.noreply.github.com>
2019-01-28 20:48:30 -0800

    Updated example file

--- packages/toolbelt/templates/koa/blossom/components/example/example.root.ts
+++ packages/toolbelt/templates/koa/blossom/components/example/example.root.ts
@@ -1,18 +1,9 @@
-import { GraphQLResolveInfo } from 'graphql';
-
-import { RequestContext, resolveArray, BlossomRootQuery } from 'blossom/instance';
+import { resolveArray, BlossomRootQuery } from 'blossom/instance';
 import { Example, ExampleQuery } from 'blossom/components/example/example.types';
 import { exampleResolver } from 'blossom/components/example/example.resolvers';
 import { exampleByName, ExampleData } from './example.sources';
-import Maybe from 'graphql/tsutils/Maybe';
 
-export const exampleRootQuery: ExampleQuery = async function exampleRootQuery(
-  args: {
-    names: ReadonlyArray<string>;
-  },
-  ctx: RequestContext,
-  ast: GraphQLResolveInfo,
-): Promise<ReadonlyArray<Maybe<Example>>> {
+export const exampleRootQuery: ExampleQuery = async function exampleRootQuery(args, ctx, ast) {
   const data = await ctx.loader(exampleByName).loadMany([...args.names]);
 
   return resolveArray<ExampleData, Example, typeof ctx>({
