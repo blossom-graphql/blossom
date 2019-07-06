dd534b0e366f1a8d1e553530bc8b7b98c9f6ea47
Sebastian Soto <spsoto@users.noreply.github.com>
2019-07-06 15:44:47 -0700

    Toolbelt: Koa Template: Bump Versions

--- packages/toolbelt/src/cmd/bootstrap/koa/koa.packages.ts
+++ packages/toolbelt/src/cmd/bootstrap/koa/koa.packages.ts
@@ -10,27 +10,27 @@ import { BLOSSOM_CORE_VERSION } from '../../../lib/constants';
 
 export default {
   dependencies: {
-    graphql: '^14.0.2',
+    graphql: '^14.4.2',
     '@blossom-gql/core': BLOSSOM_CORE_VERSION,
-    koa: '^2.6.2',
+    koa: '^2.7.0',
     'koa-bodyparser': '^4.2.1',
     'koa-router': '^7.4.0',
     '@koa/cors': '^3.0.0',
     'graphql-playground-middleware-koa': '^1.6.8',
-    'source-map-support': '^0.5.9',
+    'source-map-support': '^0.5.12',
   },
   devDependencies: {
     '@blossom-gql/toolbelt':
       '/Users/sebastiansoto/repositories/blossom/packages/toolbelt',
-    jest: '^23.3.10',
-    prettier: '^1.16.0',
-    '@types/node': `^10.12.18`,
-    '@types/graphql': '^14.0.3',
-    '@types/jest': '^23.3.10',
-    '@types/koa': '^2.0.47',
+    jest: '^24.8.0',
+    prettier: '^1.18.2',
+    '@types/node': `^12.0.12`,
+    '@types/graphql': '^14.2.2',
+    '@types/jest': '^24.0.15',
+    '@types/koa': '^2.0.49',
     '@types/koa-bodyparser': '^4.2.1',
     '@types/koa-router': '^7.0.38',
     '@types/koa__cors': '^2.2.3',
-    '@types/webpack-env': '^1.13.6',
+    '@types/webpack-env': '^1.13.9',
   },
 };
